c2b14ac92e385eabb2961193d9bfe12c0b45df70
Riyakuila <riyakuila1006@gmail.com>
2026-05-25 16:39:49 +0530

    Testimonials section added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import OurProcess from "./components/OurProcess/OurProcess";
 import Process from "./components/Process/Process";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Services />
       <Process />
       <OurProcess />
+      <Testimonials />
     </div>
   );
 }
